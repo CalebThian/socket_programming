0134587fef48d320e027b5b8c86ec2b718e87e68
Caleb Thian Jia Le <calebthian@gmail.com>
2021-03-25 06:22:44 +0800

    finish comment client.c

--- np_sample_code/tcp/message/client.c
+++ np_sample_code/tcp/message/client.c
@@ -28,44 +28,52 @@ int main(int argc, char *argv[])
 	/*Set portno from argument*/
 	portno = atoi(argv[2]);
 
-	/*Initialize sockfd by socket()*/
+	/*1. Initialize sockfd by socket()*/
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0) 
         error("ERROR opening socket");
     
-	/*Set server(in this assignment we use 'localhost')*/
+	/*a. Set server(in this assignment we use 'localhost')*/
 	server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
     
-	/*using bzero() set "serv_addr" bit of "serv_addr" to be 0-->initialize serv_addr which is a string*/
+	/*b. using bzero() set "serv_addr" bit of "serv_addr" to be 0-->initialize serv_addr which is a string*/
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 
-	/*sin_family->represent protocol, AF_UNIX for Unix OS, AF_INET for via internet, and others*/
+	/*c.i. sin_family->represent protocol, AF_UNIX for Unix OS, AF_INET for via internet, and others*/
     serv_addr.sin_family = AF_INET;
 
-	/*sin_addr-> represent ip addr, here via a number*/
+	/*c.ii. sin_addr-> represent ip addr, here via a number*/
     bcopy((char *)server->h_addr, 
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
 
-	/*sin_port->represent port number,*/
+	/*c.iii.sin_port->represent port number,*/
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    
+	/*2.Connect(socket,server address, server address,length)*/
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
+
+	/*3. Connection establish, write()*/
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
          error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
+    
+	/*4. Data sent,and some data replied, read() */
+	bzero(buffer,256);
+	n = read(sockfd,buffer,255);
     if (n < 0) 
          error("ERROR reading from socket");
     printf("%s\n",buffer);
-    close(sockfd);
+    
+	/*5.Close()*/
+	close(sockfd);
     return 0;
 }
