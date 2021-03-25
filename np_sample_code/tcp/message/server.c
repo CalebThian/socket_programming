75d5e763303c1f88bfc2316705eedd68229e33a3
Caleb Thian Jia Le <calebthian@gmail.com>
2021-03-25 20:13:40 +0800

    Finish commit on servr.c

--- np_sample_code/tcp/message/server.c
+++ np_sample_code/tcp/message/server.c
@@ -8,6 +8,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define BUFFER_SIZE 1024
+#define FILE_NAME_MAX_SIZE 512
+
 void error(const char *msg)
 {
     perror(msg);
@@ -18,31 +21,55 @@ int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno;
      socklen_t clilen;
-     char buffer[256];
+     char buffer[BUFFER_SIZE];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
+
+	 //1. Check port whether provided or not
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
+
+	 //2. Initialize sockfd
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0)
         error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
+     
+	 //3. Set up server addr
+	 //a. Initialize serv_addr by bzero()
+	 bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
+	 
+	 //b.i. set sin_family as AF_INET,which means via internet
+	 serv_addr.sin_family = AF_INET;
+
+	 //b.ii. set sin_addr as INADDR_ANY,which is 0.0.0.0, means listen to any ip connect to current port
      serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+	 //b.iii. set sin_port as given in argv
      serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+
+	 //4. bind(): bind the socket with socket address,i.e. server address
+     if (bind(sockfd,(struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0)
               error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd,
+     
+	 //5. listen(): listen to the socket
+	 listen(sockfd,5);
+
+	 //6. accept(): accept the socket that requested the connection
+     clilen = sizeof(cli_addr); 
+	 newsockfd = accept(sockfd,
                  (struct sockaddr *) &cli_addr,
                  &clilen);
      if (newsockfd < 0)
           error("ERROR on accept");
+
+	
+
+	 /*
+	 //7. read(): Read from the socket
      bzero(buffer,256);
      n = read(newsockfd,buffer,255);
      if (n < 0) error("ERROR reading from socket");
@@ -50,6 +77,7 @@ int main(int argc, char *argv[])
      n = write(newsockfd,"I got your message",18);
      if (n < 0) error("ERROR writing to socket");
      close(newsockfd);
-     close(sockfd);
+     */
+	 close(sockfd);
      return 0;
 }
