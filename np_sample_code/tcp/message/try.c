e2c082983aa27e04d037f6ed1cf0232cd37f5c42
Caleb Thian Jia Le <calebthian@gmail.com>
2021-03-26 10:55:03 +0800

    Success transfer a file larger than 200MB

--- np_sample_code/tcp/message/try.c
+++ np_sample_code/tcp/message/try.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 
 int main(){
-	int len;
-	printf("%d\n",len=90);
+	FILE *fp = fopen("alphabet.txt","w");
+	int len = 0;
+	int mb = 0;
+	char alpha[26];
+	for(int i = 0;i<26;++i){
+		alpha[i]='a'+i;
+	}
+	while(mb<300*1024){
+		len = 0;
+		while(len<1024){
+			fprintf(fp,alpha);
+			len+=26;
+		}
+		mb+=1;
+	}
+	fclose(fp);
+	return 0;
 }
